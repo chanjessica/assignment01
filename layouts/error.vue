11b54637a4987ffd1ed228ad124267716188065d
chanjessica <jchan_sea@yahoo.com>
2019-08-29 18:52:05 -0700

    error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-        <h1 v-if="error.statusCode === 404"> Page Not Found </h1>
+        <h1 v-if="error.statusCode === 404"> Sorry!   Page Not Found </h1>
         <h1 v-else> An error occurred</h1>
        <nuxt-link to="/">Home</nuxt-link>
     </div>
